15f8acc452ec600b4a857be1ef01e396fa2ec88c
Joktaa <joris.rouziere@gmail.com>
2021-03-29 15:53:30 +0200

    add vulnerable

--- main.py
+++ main.py
@@ -8,7 +8,34 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 def allowed_file(filename):
 	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+
+def unzip(zip_file, extraction_path):
+    """
+    code to unzip files
+    """
+    print "[INFO] Unzipping"
+    try:
+        files = []
+        with zipfile.ZipFile(zip_file, "r") as z:
+            for fileinfo in z.infolist():
+                filename = fileinfo.filename
+                dat = z.open(filename, "r")
+                files.append(filename)
+                outfile = os.path.join(extraction_path, filename)
+                if not os.path.exists(os.path.dirname(outfile)):
+                    try:
+                        os.makedirs(os.path.dirname(outfile))
+                    except OSError as exc:  # Guard against race condition
+                        if exc.errno != errno.EEXIST:
+                            print "\n[WARN] OS Error: Race Condition"
+                if not outfile.endswith("/"):
+                    with io.open(outfile, mode='wb') as f:
+                        f.write(dat.read())
+                dat.close()
+        return files
+    except Exception as e:
+        print "[ERROR] Unzipping Error" + str(e)
+
 @app.route('/')
 def upload_form():
 	return render_template('index.html')
